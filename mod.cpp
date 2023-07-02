68c54a9dbf778f9a3075229055ced3e723ea246c
dekken <philip.deegan@gmail.com>
2023-07-02 22:36:40 +0200

    updates

--- mod.cpp
+++ mod.cpp
@@ -28,95 +28,80 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-
-#include "maiken.hpp"
-#define KUL_EXPORT
-#undef  _KUL_DEFS_HPP_
-#include "kul/defs.hpp"
-
 #include <unordered_set>
 
-namespace mkn { namespace clang {
-class TidyModule : public maiken::Module{
-    protected:
-        static void VALIDATE_NODE(const YAML::Node& node){
-            using namespace kul::yaml;
-            Validator({
-                NodeValidator("args"),
-                NodeValidator("config"),
-                NodeValidator("checks"),
-                NodeValidator("types")
-            }).validate(node);
-        }
-    public:
-        void link(maiken::Application& a, const YAML::Node& node) KTHROW (std::exception) override{
-            VALIDATE_NODE(node);
+#include "maiken/module/init.hpp"
 
-            KLOG(DBG) << kul::env::GET("PATH");
-            KLOG(DBG) << kul::env::WHICH("gcc");
+namespace mkn {
+namespace clang {
+class TidyModule : public maiken::Module {
+ protected:
+  static void VALIDATE_NODE(const YAML::Node& node) {
+    using namespace mkn::kul::yaml;
+    Validator({NodeValidator("args"), NodeValidator("config"),
+               NodeValidator("checks"), NodeValidator("types")})
+        .validate(node);
+  }
 
-            kul::Process p("clang-tidy");
+ public:
+  void link(maiken::Application& a, const YAML::Node& node)
+      KTHROW(std::exception) override {
+    VALIDATE_NODE(node);
 
-            std::string clangHome(kul::env::GET("CLANG_HOME"));
-            if(!clangHome.empty()){
-                kul::Dir cBin("bin", clangHome);
-                if(!cBin) KEXCEPT(kul::Exception, "$CLANG_HOME/bin does not exist");
-                kul::cli::EnvVar pa(
-                    "PATH",
-                    cBin.real(),
-                    kul::cli::EnvVarMode::PREP);
-                p.var(pa.name(), pa.toString());
-            };
+    std::string binary = mkn::kul::env::GET("CLANG_TIDY", "clang-tidy");
+    mkn::kul::Process p(binary);
 
-            if(!node["config"] && !node["checks"])
-                p << "-config=''";
+    std::string clangHome(mkn::kul::env::GET("CLANG_HOME"));
+    if (!clangHome.empty()) {
+      mkn::kul::Dir cBin("bin", clangHome);
+      if (!cBin) KEXCEPT(mkn::kul::Exception, "$CLANG_HOME/bin does not exist");
+      mkn::kul::cli::EnvVar pa("PATH", cBin.real(),
+                               mkn::kul::cli::EnvVarMode::PREP);
+      p.var(pa.name(), pa.toString());
+    };
 
-            if(node["config"])
-                p << std::string("-config="+node["config"].Scalar());
+    if (!node["config"] && !node["checks"]) p << "-config=''";
 
-            if(node["checks"])
-                p << std::string("-checks="+node["checks"].Scalar());
+    if (node["config"]) p << std::string("-config=" + node["config"].Scalar());
 
-            std::unordered_set<std::string> types;
-            if(!node["types"]){
-                types = {"cpp", "cxx", "cc", "cc", "h", "hpp"};
-            }else
-                for(const auto& s : kul::String::SPLIT(node["types"].Scalar(), ":"))
-                    types.insert(s);
+    if (node["checks"]) p << std::string("-checks=" + node["checks"].Scalar());
 
-            const auto sources = a.sourceMap();
-            for(const auto& p1 : sources)
-              if(types.count(p1.first))
-                for(const auto& p2 : p1.second)
-                  for(const auto& p3 : p2.second)
-                    p << kul::File(p3).escm();
+    std::unordered_set<std::string> types;
+    if (!node["types"]) {
+      types = {"cpp", "cxx", "cc", "cc", "h", "hpp"};
+    } else
+      for (const auto& s : mkn::kul::String::SPLIT(node["types"].Scalar(), ":"))
+        types.insert(s);
 
-            p << "--";
-            if(node["args"])
-              for(const auto& s : kul::cli::asArgs(node["args"].Scalar()))
-                 p << s;
+    const auto sources = a.sourceMap();
+    for (const auto& p1 : sources)
+      if (types.count(p1.first))
+        for (const auto& p2 : p1.second)
+          for (const auto& p3 : p2.second) p << mkn::kul::File(p3.in()).escm();
 
-            for(const auto& s : a.includes()){
-              kul::Dir inc(s.first);
-              if(!inc) KEXCEPT(kul::Exception, "Directory does not exist: ")
-                << inc.path();
-              p << std::string("-I" + kul::Dir(s.first).escm());
-            }
+    p << "--";
+    if (node["args"])
+      for (const auto& s : mkn::kul::cli::asArgs(node["args"].Scalar())) p << s;
 
-            KLOG(DBG) << p;
-            p.start();
-        }
+    for (const auto& s : a.includes()) {
+      mkn::kul::Dir inc(s.first);
+      if (!inc)
+        KEXCEPT(mkn::kul::Exception, "Directory does not exist: ")
+            << inc.path();
+      p << std::string("-I" + mkn::kul::Dir(s.first).escm());
+    }
+
+    KLOG(DBG) << p;
+    p.start();
+  }
 };
-}}
+}  // namespace clang
+}  // namespace mkn
 
-extern "C"
-KUL_PUBLISH
-maiken::Module* maiken_module_construct() {
-    return new mkn :: clang :: TidyModule;
+extern "C" KUL_PUBLISH maiken::Module* maiken_module_construct() {
+  return new mkn ::clang ::TidyModule;
 }
 
-extern "C"
-KUL_PUBLISH
-void maiken_module_destruct(maiken::Module* p) {
-    delete p;
+extern "C" KUL_PUBLISH void maiken_module_destruct(maiken::Module* p) {
+  delete p;
 }
--- test.cpp
+++ test.cpp
@@ -1,36 +1,33 @@
 
 
-#include "kul/signal.hpp"
-
 #include <maiken.hpp>
+
+#include "mkn/kul/signal.hpp"
 
 const std::string yArgs = R"(
 args:
-    -std=c++14
+    -std=c++17
 )";
 
 int main(int argc, char* argv[]) {
-    kul::Signal sig;
-    try{
-        YAML::Node node = kul::yaml::String(yArgs).root();
-        char * argv2[2] = {argv[0], (char*) "-R"};
-        auto& app(maiken::Application::CREATE(2, argv2));
-        auto loader(maiken::ModuleLoader::LOAD(app));
-        loader->module()->link(app, node);
-        loader->unload();
-    }
-    catch(const kul::Exception& e){ 
-        KERR << e.what(); 
-        return 2; 
-    }
-    catch(const std::exception& e){ 
-        KERR << e.what(); 
-        return 3; 
-    }    
-    catch(...){ 
-        KERR << "UNKNOWN EXCEPTION TYPE CAUGHT"; 
-        return 5; 
-    }
+  mkn::kul::Signal sig;
+  try {
+    YAML::Node node = mkn::kul::yaml::String(yArgs).root();
+    char* argv2[2] = {argv[0], (char*)"-R"};
+    auto app = (maiken::Application::CREATE(2, argv2))[0];
+    auto loader(maiken::ModuleLoader::LOAD(*app));
+    loader->module()->link(*app, node);
+    loader->unload();
+  } catch (mkn::kul::Exception const& e) {
+    KERR << e.what();
+    return 2;
+  } catch (std::exception const& e) {
+    KERR << e.what();
+    return 3;
+  } catch (...) {
+    KERR << "UNKNOWN EXCEPTION TYPE CAUGHT";
+    return 5;
+  }
 
-    return 0;
+  return 0;
 }
